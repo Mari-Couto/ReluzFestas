0cb7d3a9f699c1b7bd05ee2e35abe79fbc7ef960
Marina Couto <marinacoutonina@gmail.com>
2024-01-03 18:13:56 -0300

    construindo navbar

---
+++ script.js
@@ -0,0 +1,24 @@
+window.addEventListener("scroll", function () {
+    const navbar = document.querySelector(".navbar");
+    if (window.scrollY > 50) {
+        navbar.classList.add("scrolled");
+    } else {
+        navbar.classList.remove("scrolled");
+    }
+});
+
+
+    document.addEventListener('DOMContentLoaded', function () {
+        const menuBtn = document.querySelector('.menu-btn');
+        const navLinks = document.querySelector('.nav-links');
+
+        menuBtn.addEventListener('click', function () {
+            navLinks.classList.toggle('visible'); 
+        });
+
+       
+        navLinks.addEventListener('click', function () {
+            navLinks.classList.remove('visible');
+        });
+    });
+
