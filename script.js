01ece0ff205c9551b25d44ed5b0004375644c8f8
Marina Couto <marinacoutonina@gmail.com>
2024-01-03 19:28:32 -0300

    bug-menuNavbar

--- script.js
+++ script.js
@@ -8,17 +8,19 @@ window.addEventListener("scroll", function () {
 });
 
 
-    document.addEventListener('DOMContentLoaded', function () {
-        const menuBtn = document.querySelector('.menu-btn');
-        const navLinks = document.querySelector('.nav-links');
+document.addEventListener('DOMContentLoaded', function () {
+    const menuBtn = document.querySelector('.menu-btn');
+    const navLinks = document.querySelector('.nav-links');
 
-        menuBtn.addEventListener('click', function () {
-            navLinks.classList.toggle('visible'); 
-        });
-
-       
-        navLinks.addEventListener('click', function () {
-            navLinks.classList.remove('visible');
-        });
+    menuBtn.addEventListener('click', function () {
+        navLinks.classList.toggle('visible');
     });
+
+    // Adicione este event listener para fechar o menu ao clicar em um link
+    navLinks.addEventListener('click', function (event) {
+        if (event.target.tagName === 'A') {
+            navLinks.classList.remove('visible');
+        }
+    });
+});
 
