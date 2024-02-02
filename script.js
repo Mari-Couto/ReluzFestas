8206a6450577134c9f04937728516fd7c177e070
Marina Couto <marinacoutonina@gmail.com>
2024-02-02 17:44:12 -0300

    removendo carrossel

--- script.js
+++ script.js
@@ -23,21 +23,3 @@ document.addEventListener('DOMContentLoaded', function () {
         }
     });
 });
-
-//carroussel
-
-let count = 1;
-document.getElementById("radio1").checked = true;
-
-setInterval( function(){
-    nextImage()
-}, 4000)
-
-function nextImage(){
-    count++;
-    if(count>4){
-        count = 1;
-    }
-    document.getElementById("radio"+count).checked = true;
-}
-
